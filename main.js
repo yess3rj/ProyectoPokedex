3be844f7e4afe90add11c63ac97c8e53354c00c8
Yessenia Reyes <yessenia.reyes.juarez@gmail.com>
2022-06-22 21:56:02 -0500

    feat: visualizacion mejorada

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ const pokeImgContainer = document.querySelector('[data-poke-img-container]');
 const pokeId = document.querySelector('[data-poke-id]');
 const pokeTypes = document.querySelector('[data-poke-types]');
 const pokeStats = document.querySelector('[data-poke-stats]');
+const pokeExperience = document.querySelector('[data-poke-experience]');
+//const pokeHeight = document.querySelector('[data-poke-height]')
+//const pokeWeight = document.querySelector('[data-poke-weight]')
 
 const typeColors = {
     electric: '#EFB20A',
-    normal: '#509218',
+    normal: '#8DA813',
     fire: '#E43E05',
     water: '#7DBAF2',
     ice: '#67C8E9',
@@ -22,10 +25,10 @@ const typeColors = {
     ground: '#C3A54D',
     dragon: '#7862D9',
     steel: '#ABABB8',
-    fighting: '#509218',
+    fighting: '#9E9711',
     fairy: '#EAB1EA',
     dark: '#392B21',
-    default: '#2A1A1F',
+    default: '#C2788F',
 };
 
 
@@ -39,12 +42,16 @@ const searchPokemon = event => {
 }
 
 const renderPokemonData = data => {
-    const sprite = data.sprites.front_default;
+    const sprite = data.sprites.other["official-artwork"].front_default
+    //const sprite = data.sprites.front_default;
     const { stats, types } = data;
     
     pokeName.textContent = data.name;
     pokeImg.setAttribute('src', sprite);
     pokeId.textContent = `Nª ${data.id}`
+    pokeExperience.textContent = `Experience: ${data.base_experience}`
+    //pokeHeight.textContent = `Height: ${data.height}`
+    //pokeWeight.textContent = `Weight: ${data.weight}`
     setCardColor(types);
     renderPokemonTypes(types);
     renderPokemonStats(stats);
@@ -54,7 +61,7 @@ const setCardColor = types => {
     const colorOne = typeColors[types[0].type.name];
     const colorTwo = types [1] ? typeColors[types[1].type.name] : typeColors.default;
     pokeImg.style.background = `radial-gradient(${colorTwo} 33%, ${colorOne} 33%)`;
-    pokeImg.style.backgroundSize = '5px 5px';
+    pokeImg.style.backgroundSize = '10px 10px';
 }
 
 const renderPokemonTypes = types => {
@@ -82,10 +89,50 @@ const renderPokemonStats = stats => {
 }
 
 const renderNotFound = () => {
-    pokeName.textContent = 'Nombre o ID no válido, intenta nuevamente';
+    pokeName.textContent = 'Invalid name or ID, please try again';
     pokeImg.setAttribute('src', './img/shadow.png');
-    pokeImg.style.background = '#fff';
+    pokeImg.style.background = '#f4ecf4';
     pokeTypes.innerHTML = '';
     pokeStats.innerHTML = '';
     pokeId.innerHTML = '';
+    pokeExperience.innerHTML = '';
 }
+
+//////Mostrar 150 pokemones
+const pokedex = document.getElementById('pokedex');
+const fetchPokemon = () => {
+    const promises = [];
+    for(let i=1; i<=898; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((result) => ({
+            name: result.name,
+            image: result.sprites.other["official-artwork"].front_default,
+            type: result.types.map((type) =>type.type.name).join(', '),
+            id: result.id,
+            experience: result.base_experience
+        }));
+        displayPokemon(pokemon);
+    });
+}
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon
+    .map(
+        (pokeman) => `
+    <li class="card">
+        <img class="card-image" src="${pokeman.image}"/>
+        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+        <p class="card-subtitle">Type: ${pokeman.type}</p>
+        <p class="card-subtitle">Experience: ${pokeman.experience}</p>
+    </li>
+        `
+    )
+    .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
